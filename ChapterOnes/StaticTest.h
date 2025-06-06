1e5afabe12e14e580dcd5755dc16b726abb05a09
Sanao2 <SanaoAiza1102@gmail.com>
2025-06-06 22:37:45 +0900

    # 챕터 1 ~ 4 정리

--- ChapterOnes/StaticTest.h
+++ ChapterOnes/StaticTest.h
@@ -1,13 +1,15 @@
 #pragma once
 
-
-
-
 class StaticTest
 {
 public:
 	int a = 0;
 	static int num;
+	StaticTest& stest(int a)
+	{
+		static StaticTest st;
+		return st; 
+	}
 	StaticTest()
 	{
 		a = 0; 
